d4d87dcd9520b37807bbcde61b8fc3856999eb3d
sk-technology801 <sardarsaadisaadi@gmail.com>
2025-07-04 01:15:38 +0500

    gg

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) throw new Error('⚠️ Please define MONGODB_URI in .env.local');
+if (!MONGODB_URI) throw new Error("⚠️ Please define MONGODB_URI in .env.local");
 
 let cached = global.mongoose || { conn: null, promise: null };
 
@@ -11,9 +11,11 @@ async function connectDB() {
 
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: 'codenesttechnologies',
       bufferCommands: false,
     }).then((mongoose) => mongoose);
   }
+
   cached.conn = await cached.promise;
   return cached.conn;
 }
